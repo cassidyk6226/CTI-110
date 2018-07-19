1a3c0975bd2d0ac03c915f2da8e4394491e696d2
cassidyk6226 <41083881+cassidyk6226@users.noreply.github.com>
2018-07-19 15:54:19 -0400

    Add files via upload

---
+++ P4T2_ Bug Collector_CASSIDY.py
@@ -0,0 +1,20 @@
+#CTI-110
+#P4T2 - BUG COLLECTOR
+#KYLE CASSIDY
+#JULY 19,2018
+
+#Initialize the accumulator
+total = 0
+
+#Get bugs collected for each day.
+for day in range(1,8):
+
+#Prompt the User.
+      print("Enter the bugs collected on day",day)
+
+#Input the number of bugs.
+      bugs = int(input())
+      total = total = bugs
+
+#Display the total of bugs.
+      print("You collected a total of", total,"bugs.")
